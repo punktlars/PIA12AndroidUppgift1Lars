dd6db182cc31b7225428159d62ed27f070567621
Lars Berggren <punktlars@gmail.com>
2024-02-02 10:20:39 +0100

    PIA12: Uppgift 1 Android

---
+++ app/src/main/java/com/example/pia12androiduppgift1lars/WelcomeFragment.kt
@@ -0,0 +1,45 @@
+package com.example.pia12androiduppgift1lars
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pia12androiduppgift1lars.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private var _binding : FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.welcomeText.text = "Welcome"
+
+        binding.actionBtn.setOnClickListener {
+            binding.welcomeText.text = "Good bye"
+            binding.welcomeText.setBackgroundColor(Color.RED)
+        }
+    }
+
+
+
+}
